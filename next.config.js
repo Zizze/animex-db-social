9e9eeed910863af43c4531cff998e242522e0de5
Alex Alexa <vjvjpchela@gmail.com>
2023-04-14 15:59:42 +0300

    Added responsive and adaptive styles for home page and other fixes.

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
 				protocol: "https",
 				hostname: "cdn.myanimelist.net",
 				port: "",
-				pathname: "/images/anime/**",
+				pathname: "/**",
 			},
 			{
 				protocol: "https",
--- src/components/screens/home/items/item/Item.tsx
+++ src/components/screens/home/items/item/Item.tsx
@@ -1,9 +1,12 @@
 import { IDataAnime } from "@Store/animeJikan/IAnime.interface";
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import classes from "./Item.module.scss";
 import Link from "next/link";
 import { IAnimeFirebase } from "@/types/types";
 import { useRouter } from "next/router";
+import Image, { StaticImageData } from "next/image";
+import Loading from "@Components/UI/loading/Loading";
+import defaultImage from "@Public/testava.jpg";
 
 interface IProps {
 	anime: IDataAnime | IAnimeFirebase;
@@ -14,6 +17,11 @@ const Item: FC<IProps> = ({ anime, personalRate }) => {
 	const { asPath } = useRouter();
 	const { title: name, images, mal_id: id, episodes, score } = anime;
 
+	const [isLoading, setIsLoading] = useState(true);
+	const [image, setImage] = useState<string | StaticImageData>(images.jpg.large_image_url);
+	const [smallImage, setSmallImage] = useState<string | StaticImageData>(images.jpg.image_url);
+
+	console.log("render");
 	return (
 		<li className={classes.anime}>
 			{personalRate && asPath.includes("my-list") && (
@@ -23,11 +31,27 @@ const Item: FC<IProps> = ({ anime, personalRate }) => {
 			)}
 			<Link href={`/anime/${id}`} className={classes.link}>
 				<div className={classes.backImg}>
-					<img src={images.jpg.image_url} />
+					<Image src={smallImage} alt="Anime background" width={250} height={250} priority={true} />
 					<div></div>
 				</div>
 				<div className={classes.mainBlock}>
-					<img src={images.jpg.image_url} alt={name} />
+					<div className={classes.image}>
+						<Image
+							src={image}
+							alt={`${name} image`}
+							width={500}
+							height={250}
+							priority={true}
+							onLoadingComplete={() => setIsLoading(false)}
+							onError={() => {
+								setImage(defaultImage);
+								setSmallImage(defaultImage);
+								setIsLoading(false);
+							}}
+						/>
+						{isLoading && <Loading className={classes.loadImage} />}
+					</div>
+
 					<div className={classes.flex}>
 						<h4 className={classes.name} title={name}>
 							{name}
